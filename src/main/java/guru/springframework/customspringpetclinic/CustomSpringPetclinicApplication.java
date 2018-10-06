0d2b073564ac36590b6fa92590d3debd14d97432
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-06 21:07:44 +0200

    first commit

---
+++ src/main/java/guru/springframework/customspringpetclinic/CustomSpringPetclinicApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.customspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomSpringPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomSpringPetclinicApplication.class, args);
+    }
+}
