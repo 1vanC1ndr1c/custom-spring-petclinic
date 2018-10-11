70ce18e81df926c0cf97a0420e42e1033b284641
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-11 18:41:24 +0200

    Refactor services. Closes #12

---
+++ pet-clinic-data/src/main/java/customspringpetclinic/services/CrudService.java
@@ -0,0 +1,18 @@
+package customspringpetclinic.services;
+
+import java.util.Set;
+
+//interface that mimics spring data repository
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+
+}
--- pet-clinic-data/src/main/java/customspringpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/customspringpetclinic/services/VetService.java
@@ -2,13 +2,7 @@ package customspringpetclinic.services;
 
 import customspringpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
 
