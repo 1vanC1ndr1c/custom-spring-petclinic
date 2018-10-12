47b6aa3f9e8eb16309902d7fdaa49f3dcc31bbf8
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-12 17:17:36 +0200

    Implement auto generated ID. Close #24

--- pet-clinic-data/src/main/java/customspringpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/customspringpetclinic/services/map/OwnerServiceMap.java
@@ -26,9 +26,8 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
         super.delete(object);
     }
 
-    @Override
     public Owner save(Owner object) {
-        return super.save(object.getId(), object);
+        return super.save(object);
     }
 
     @Override
