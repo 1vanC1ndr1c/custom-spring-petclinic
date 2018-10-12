059aa908574439fe2b158b2ef2008793dc8626a3
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-12 16:54:41 +0200

    List owners on their index pages. Formated owner index page. Close #17

--- pet-clinic-web/src/main/java/customspringpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/customspringpetclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package customspringpetclinic.controllers;
 
+import customspringpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")//this one combines with the one below
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/", "", "Index", "Index.html", "index", "index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {//by adding Model, Spring MVC will automatically add when method is called
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
