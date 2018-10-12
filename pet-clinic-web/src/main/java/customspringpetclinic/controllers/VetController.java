d0ef724bd9f5afced6365413335db47b36c8a2a0
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-12 17:04:39 +0200

    List vets on their index pages. Formated vets index page. Close #16

--- pet-clinic-web/src/main/java/customspringpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/customspringpetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package customspringpetclinic.controllers;
 
+import customspringpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/Index.html","/vets/index.html","/vets/Index"})
-    public String listVets() {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/Index.html", "/vets/index.html", "/vets/Index"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";//important
     }
 }
