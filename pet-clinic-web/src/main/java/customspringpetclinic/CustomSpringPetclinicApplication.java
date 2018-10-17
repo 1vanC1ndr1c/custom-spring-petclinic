f4ae0aa4f28b3b43c0d63f9a5abcc6a9f1f83a28
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-17 19:11:00 +0200

    Close #18

--- pet-clinic-web/src/main/java/customspringpetclinic/CustomSpringPetclinicApplication.java
+++ pet-clinic-web/src/main/java/customspringpetclinic/CustomSpringPetclinicApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomSpringPetclinicApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(CustomSpringPetclinicApplication.class, args);
     }
 }
