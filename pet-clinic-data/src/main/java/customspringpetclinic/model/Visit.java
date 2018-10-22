a757b7e2541ac7a4082e8ae63d1171d96fb129be
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-22 22:58:52 +0200

    Convert Visit to JPA entities. Close #39

--- pet-clinic-data/src/main/java/customspringpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/customspringpetclinic/model/Visit.java
@@ -1,11 +1,21 @@
 package customspringpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    //inverse of relationship from 'pet'
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
