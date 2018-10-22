adf701e5b79be9bdbf145510f4c905db17bb8529
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-22 19:09:12 +0200

    Implement Base Entity. Does not work right now. Close #36

--- pet-clinic-data/src/main/java/customspringpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/customspringpetclinic/model/BaseEntity.java
@@ -1,10 +1,17 @@
 package customspringpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 //Serializable = turning objects into bytes
+@MappedSuperclass//other classes gonna be inheriting this class, but we don't need this one specifically
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
