34e88d14b4ff8f79e6f356bdea6fd0c02be28799
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-27 15:44:31 +0200

    Add PetController. #57

--- pet-clinic-web/src/main/java/customspringpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/customspringpetclinic/controllers/PetController.java
@@ -4,6 +4,7 @@ import customspringpetclinic.model.Owner;
 import customspringpetclinic.model.PetType;
 import customspringpetclinic.repositories.OwnerRepository;
 import customspringpetclinic.repositories.PetRepository;
+import customspringpetclinic.services.OwnerService;
 import customspringpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
-import java.util.Optional;
 
 /**
  * Created by Ivan Cindric for custom-spring-petclinic
@@ -26,12 +26,12 @@ public class PetController {
 
     private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdateOwnerForm";
     private final PetRepository petService;
-    private final OwnerRepository ownerService;
+    private final OwnerService ownerService;
     private final PetTypeService petTypeService;
 
-    public PetController(PetRepository petService, OwnerRepository ownerService, PetTypeService petTypeService) {
+    public PetController(PetRepository petService, OwnerRepository ownerService, OwnerService ownerService1, PetTypeService petTypeService) {
         this.petService = petService;
-        this.ownerService = ownerService;
+        this.ownerService = ownerService1;
         this.petTypeService = petTypeService;
     }
 
@@ -42,7 +42,7 @@ public class PetController {
     }
 
     @ModelAttribute("owner")
-    public Optional<Owner> findOwner(@PathVariable("ownerId") Long ownerId) {
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
         return ownerService.findById(ownerId);
     }
 
