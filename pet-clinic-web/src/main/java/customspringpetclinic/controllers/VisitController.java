426165dfad45a44dfe288263879ca57d7c12a55f
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-27 17:11:16 +0200

    Add update or add new visit to owner. Validation not currently working. Close #58

---
+++ pet-clinic-web/src/main/java/customspringpetclinic/controllers/VisitController.java
@@ -0,0 +1,65 @@
+package customspringpetclinic.controllers;
+
+import customspringpetclinic.model.Pet;
+import customspringpetclinic.model.Visit;
+import customspringpetclinic.services.PetService;
+import customspringpetclinic.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+/**
+ * Created by Ivan Cindric for custom-spring-petclinic
+ * 27-Oct-18
+ * 16:50
+ **/
+
+@Controller
+class VisitController {
+
+    private final VisitService visitService;
+    private final PetService petService;
+
+    VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable("petId") Long petId, Map<String, Object> model) {
+        Pet pet = petService.findById(petId);
+        model.put("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);//manage both sides of relationships
+
+        return visit;
+    }
+
+    // Spring MVC calls method loadPetWithVisit(...) before initNewVisitForm is called
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable("petId") Long petId, Map<String, Object> model) {
+        return "pets/createOrUpdateVisitForm";
+    }
+
+    // Spring MVC calls method loadPetWithVisit(...) before processNewVisitForm is called
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(@Valid Visit visit, BindingResult result) {
+        if (result.hasErrors()) {
+            return "pets/createOrUpdateVisitForm";
+        } else {
+            visitService.save(visit);
+            return "redirect:/owners/{ownerId}";
+        }
+    }
+
+}
